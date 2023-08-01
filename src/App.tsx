6a978a1571c20f92d497edf72f1b614e34056570
Mohammad Aljafar <xqg1122@gmail.com>
2023-08-02 02:27:24 +0300

    Learn how to use the useEffect hook and make http requests with axios library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import {useEffect, useRef, useState} from 'react';
 import './App.css';
+import axios, {AxiosError} from 'axios';
 
 function App() {
-	return <div></div>;
+	const [users, setUsers] = useState<{name: string; id: number}[]>([]);
+
+	const inputRef = useRef<HTMLInputElement>(null);
+	useEffect(() => {
+		axios
+			.get('https://jsonplaceholder.typicode.com/users')
+			.then(res => setUsers(res.data))
+			.catch((err: AxiosError) => console.log(err.response?.status));
+	}, []);
+	return (
+		<div>
+			<ul className='list-group'>
+				{users.map(u => (
+					<li key={u.id}>{u.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
